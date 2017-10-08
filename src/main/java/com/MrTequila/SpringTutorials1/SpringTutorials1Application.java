4ef562c93f20c51889292161dfa993d2ad0e5f3b
MrTequila <igi444@gmail.com>
2017-10-08 17:03:15 +0200

    First tutorial for Spring

---
+++ src/main/java/com/MrTequila/SpringTutorials1/SpringTutorials1Application.java
@@ -0,0 +1,12 @@
+package com.MrTequila.SpringTutorials1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTutorials1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTutorials1Application.class, args);
+	}
+}
