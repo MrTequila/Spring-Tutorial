1d1fbae52986a938de0840234e613d3edcc75711
MrTequila <igi444@gmail.com>
2017-10-10 17:45:51 +0200

    Tutorial 3, Accessing data with MySQL

---
+++ src/main/java/com/MrTequila/SpringTutorials3/SpringTutorials3Application.java
@@ -0,0 +1,13 @@
+package com.MrTequila.SpringTutorials3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringTutorials3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTutorials3Application.class, args);
+    }
+}
