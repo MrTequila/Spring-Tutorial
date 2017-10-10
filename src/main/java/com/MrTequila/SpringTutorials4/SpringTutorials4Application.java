f5719af4f9fecaeca8a0ad8cc820bd3f8f3ec85d
MrTequila <igi444@gmail.com>
2017-10-10 18:38:07 +0200

    Tutorial 4, Validating Form Input

---
+++ src/main/java/com/MrTequila/SpringTutorials4/SpringTutorials4Application.java
@@ -0,0 +1,13 @@
+package com.MrTequila.SpringTutorials4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringTutorials4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTutorials4Application.class, args);
+    }
+}
