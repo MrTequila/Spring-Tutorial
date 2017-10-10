6574baf23567c1875bbd5f8ce571c740b295aaff
MrTequila <igi444@gmail.com>
2017-10-10 21:26:57 +0200

    Tutorial 5, Handling form submission

---
+++ src/main/java/com/MrTequila/SpringTutorials5/SpringTutorials5Application.java
@@ -0,0 +1,13 @@
+package com.MrTequila.SpringTutorials5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringTutorials5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTutorials5Application.class, args);
+    }
+}
