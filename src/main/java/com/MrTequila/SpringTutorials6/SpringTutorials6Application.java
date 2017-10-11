166ece34d1298fe26eccdeae0112fe12b8d2b621
MrTequila <igi444@gmail.com>
2017-10-11 22:04:03 +0200

    Tutorial 6, uploading files

---
+++ src/main/java/com/MrTequila/SpringTutorials6/SpringTutorials6Application.java
@@ -0,0 +1,25 @@
+package com.MrTequila.SpringTutorials6;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class SpringTutorials6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTutorials6Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
+}
